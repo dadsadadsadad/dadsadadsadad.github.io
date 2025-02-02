dd967507d915434cf35c5dc570f846bbe19aecd6
dadsadadsadad <rafaelle.raprap@gmail.com>
2025-02-02 14:23:58 +0800

    added react router dom

---
+++ portfolio/src/components/FirstPage.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
+const FirstPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <button onClick={() => navigate("/main")}>Go to Main Page</button>
+        </div>
+    );
+};
+
+export default FirstPage;
